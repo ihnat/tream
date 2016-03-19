118a9f73d268246ef5f5163bed313943d1b8533a
ihnat <ya.ihnat@gmail.com>
2016-03-19 13:43:38 +0300

    Changed names to Tream

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,11 +22,11 @@ class Header extends Component {
           <Navigation className={s.nav} />
           <Link className={s.brand} to="/">
             <img src={require('./logo-small.png')} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
+            <span className={s.brandTxt}>Tream</span>
           </Link>
           <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
+            <h1 className={s.bannerTitle}>Tream</h1>
+            <p className={s.bannerDesc}>Complex HR process made easy</p>
           </div>
         </div>
       </div>
